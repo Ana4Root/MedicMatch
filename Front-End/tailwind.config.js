f7e05307339ebf4781a10d2b4fcbae5b201349e7
Ana <hollsteinnunes@gmail.com>
2023-09-27 21:21:33 -0300

    frontend

---
+++ Front-End/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
